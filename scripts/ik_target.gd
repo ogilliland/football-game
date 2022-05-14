1493499eecabb6f90db5e5b3b83d06b0ec41a3b6
Oliver Gilliland <omgilliland@gmail.com>
2022-05-14 09:26:04 +0100

    Dynamic character animation

--- scripts/ik_target.gd
+++ scripts/ik_target.gd
@@ -1,10 +1,10 @@
 extends Position3D
 
-var start_pos: Vector3
-
-func _ready() -> void:
-	start_pos = translation
-
-func _physics_process(_delta: float) -> void:
-	translation.y = start_pos.y + 0.2 * sin(OS.get_ticks_msec() * 0.001)
-	translation.z = start_pos.z + 0.2 * cos(OS.get_ticks_msec() * 0.001)
+#var start_pos: Vector3
+#
+#func _ready() -> void:
+#	start_pos = translation
+#
+#func _physics_process(_delta: float) -> void:
+#	translation.y = start_pos.y + 0.2 * sin(OS.get_ticks_msec() * 0.001)
+#	translation.z = start_pos.z + 0.2 * cos(OS.get_ticks_msec() * 0.001)
