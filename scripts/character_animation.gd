a71cb8da6d6e072895d171b4fc6a6bb7b12dfc59
Oliver Gilliland <omgilliland@gmail.com>
2022-05-14 19:35:04 +0100

    Lean forward while running

--- scripts/character_animation.gd
+++ scripts/character_animation.gd
@@ -4,3 +4,4 @@ func update(intensity: float) -> void:
 	set("parameters/blend_move/blend_position", intensity*2.0 - 1.0)
 	set("parameters/time_scale/scale", intensity*3.0 + 1.0)
 	set("parameters/blend_arms/blend_amount", intensity)
+	set("parameters/blend_lean/blend_position", intensity*2.0 - 1.0)
