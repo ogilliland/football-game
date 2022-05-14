ac74015a1e47123c922cff456bb846ad20cf08f5
Oliver Gilliland <omgilliland@gmail.com>
2022-05-14 19:09:24 +0100

    Add simple shadows

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,6 +3,9 @@ extends RigidBody
 var target: Node
 var target_pos: Vector3
 
+onready var shadow = $Shadow
+onready var raycast = $RayCast
+
 func set_target(new_target: Node) -> void:
 	target = new_target
 	collision_layer = 2
@@ -16,6 +19,12 @@ func clear_target() -> void:
 	sleeping = false
 
 func _physics_process(delta: float) -> void:
+	var shadow_pos = raycast.get_collision_point()
+	var height = clamp((global_transform.origin - shadow_pos).y - 0.125, 0.0, 10.0)
+	shadow.scale.x = 0.5 + height*0.05
+	shadow.scale.y = 0.5 + height*0.05
+	shadow.translation.y = -0.1 - height
+	
 	if target:
 		target_pos = target.global_transform.origin
 		translation = lerp(translation, target_pos, 10.0*delta)
